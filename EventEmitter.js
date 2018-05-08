99a38235a135bae74d18b1eb2298227758563443
Ryan Dulac <analyticrye@gmail.com>
2018-05-08 01:39:50 -0400

    event emit

---
+++ EventEmitter.js
@@ -0,0 +1,12 @@
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter();
+
+//register a listener
+emitter.on('messageLogged', function() {
+    console.log('Listener called');
+
+});
+//raise an event
+emitter.emit('messageLogged');
+
